deab890d2ce6f8c42556777e7637e129318377b5
Socheat Sok <alex@socheat.net>
2020-05-15 14:11:14 +0700

    add test script to package.json

---
+++ tests/src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    config: {
+        key: 'value',
+        production: process.env.NODE_ENV === 'production' ? true : false
+    }
+}
--- tests/webpack.config.js
+++ tests/webpack.config.js
@@ -3,30 +3,37 @@
 const GenerateJsonPlugin = require('../lib/index');
 
 module.exports = {
-    plugins: [new GenerateJsonPlugin({
-        filename: 'manifest.json',
-        space: 4,
-        value: {
-            short_name: 'Weather',
-            name: 'Weather: Do I need an umbrella?',
-            description: 'Weather forecast information',
-            icons: [
-                {
-                    src: "/images/icons-192.png",
-                    type: "image/png",
-                    sizes: "192x192"
-                },
-                {
-                    src: "/images/icons-512.png",
-                    type: "image/png",
-                    sizes: "512x512"
-                }
-            ],
-            start_url: "/?source=pwa",
-            background_color: "#3367D6",
-            display: "standalone",
-            scope: "/",
-            theme_color: "#3367D6"
-        }
-    })],
+    plugins: [
+        new GenerateJsonPlugin({
+            filename: 'manifest.json',
+            space: 4,
+            value: {
+                short_name: 'Weather',
+                name: 'Weather: Do I need an umbrella?',
+                description: 'Weather forecast information',
+                icons: [
+                    {
+                        src: "/images/icons-192.png",
+                        type: "image/png",
+                        sizes: "192x192"
+                    },
+                    {
+                        src: "/images/icons-512.png",
+                        type: "image/png",
+                        sizes: "512x512"
+                    }
+                ],
+                start_url: "/?source=pwa",
+                background_color: "#3367D6",
+                display: "standalone",
+                scope: "/",
+                theme_color: "#3367D6"
+            }
+        }),
+        new GenerateJsonPlugin({
+            filename: 'config.json',
+            space: 4,
+            source: __dirname + '/src/config.js'
+        })
+    ],
 };
